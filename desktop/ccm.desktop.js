49f169cf7b111d66c84a65f1dac87bc97984faf9
Dennis Schmidt <Dennis@yourGrigor.com>
2017-06-23 02:10:12 +0200

    Removed jsx attempts and added proper config.

--- desktop/ccm.desktop.js
+++ desktop/ccm.desktop.js
@@ -5,36 +5,66 @@
 ( function () {
 
     var ccm_version = '8.0.0';
-    var ccm_url     = 'https://akless.github.io/ccm/version/ccm-8.0.0.min.js';
+    var ccm_url = 'https://akless.github.io/ccm/version/ccm-8.0.0.min.js';
 
     var component_name = 'desktop';
-    var component_obj  = {
+    var component_obj = {
 
         name: component_name,
 
         config: {
+            apps: [
+                {
+                    name: 'ccm-quiz',
+                    url: 'https://github.com/akless/ccm-components/blob/master/quiz/ccm.quiz.js' // Beispiel
+                }
+            ]
+            ,
+            html_templates: {
+                'ccm-desktop-app': {
+                    inner: {
+                        div: {
+                            class: 'ccm-desktop-app',
+                            inner: {
+                                'img': {
+                                    src: '%img_url%'
+                                }
+                            }
+                        }
+                    }
+                },
+                'ccm-desktop-add': {
+                    inner: {
+                        div: {
+                            class: 'ccm-desktop-add',
+                            inner: {
+                                'img': {
+                                    src: '%img_url%'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         },
 
         Instance: function () {
 
-            import App from 'ccm.desktop.app';
-
             var self = this;
             var my;
 
-            self.ccm_app = App;
-            
             this.init = function (callback) {
 
-                self.ccm.helper.makeIterable( self.node.children ).map( function ( tag ) {
+                self.ccm.helper.makeIterable(self.node.children).map(function (tag) {
 
-                    switch(tag.tagName){
+                    switch (tag.tagName) {
                         case 'CCM-DESKTOP-APP':
-                            var app = self.ccm_app("http://via.placeholder.com/400x400");
-                            console.log(app.render());
+                            var conf = self.ccm.helper.generateConfig( tag )
+                            console.log(conf);
                             break;
                         case 'CCM-DESKTOP-ADD':
-                            console.log('ADD');
+                            var conf = self.ccm.helper.generateConfig( tag )
+                            console.log(conf);
                             break;
                     }
 
@@ -57,7 +87,18 @@
         }
     };
 
-    var namespace = window.ccm && ccm.components[ component_name ]; if ( namespace ) { if ( namespace.ccm_version ) ccm_version = namespace.ccm_version; if ( namespace.ccm_url ) ccm_url = namespace.ccm_url; }
-    if ( !window.ccm || !ccm[ ccm_version ] ) { var tag = document.createElement( 'script' ); document.head.appendChild( tag ); tag.onload = register; tag.src = ccm_url; } else register();
-    function register() { ccm[ ccm_version ].component( component_obj ); }
+    var namespace = window.ccm && ccm.components[component_name];
+    if (namespace) {
+        if (namespace.ccm_version) ccm_version = namespace.ccm_version;
+        if (namespace.ccm_url) ccm_url = namespace.ccm_url;
+    }
+    if (!window.ccm || !ccm[ccm_version]) {
+        var tag = document.createElement('script');
+        document.head.appendChild(tag);
+        tag.onload = register;
+        tag.src = ccm_url;
+    } else register();
+    function register() {
+        ccm[ccm_version].component(component_obj);
+    }
 }() );
